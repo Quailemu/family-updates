2fb00920ae136e35937fe241513872684e41c888
Quailemu <kateshadwell@hotmail.co.uk>
2026-04-18 15:42:47 +0100

    Fix operating_mode migration order to avoid check-constraint violation

--- supabase/migrations/0027_operating_mode_context_values.sql
+++ supabase/migrations/0027_operating_mode_context_values.sql
@@ -3,6 +3,9 @@
 --   care_home  -> care_organisation
 --   family_led -> personal_use
 
+alter table public.care_homes
+  drop constraint if exists care_homes_operating_mode_check;
+
 update public.care_homes
 set operating_mode = 'care_organisation'
 where operating_mode = 'care_home';
@@ -13,9 +16,6 @@ where operating_mode = 'family_led';
 
 alter table public.care_homes
   alter column operating_mode set default 'care_organisation';
-
-alter table public.care_homes
-  drop constraint if exists care_homes_operating_mode_check;
 
 alter table public.care_homes
   add constraint care_homes_operating_mode_check
